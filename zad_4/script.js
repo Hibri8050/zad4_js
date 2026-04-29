fe12a0ba44cd43e595822661c45f968550ae4d70
Dmytro Datsenko <ddatsen1@stu.vistula.edu.pl>
2026-04-29 16:38:31 +0200

    Update script.js

--- zad_4/script.js
+++ zad_4/script.js
@@ -27,3 +27,35 @@ toggleBtn.addEventListener('click', () => {
     targetSection.classList.toggle('hidden');
     console.log("Widoczność sekcji zmieniona");
 });
+const contactForm = document.getElementById('contactForm');
+const validationMessage = document.getElementById('validationMessage');
+
+contactForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    validationMessage.innerText = "";
+    validationMessage.className = "";
+
+    const name = document.getElementById('userName').value;
+    const surname = document.getElementById('userSurname').value;
+    const email = document.getElementById('userEmail').value;
+
+    const hasDigits = /\d/;
+
+    if (hasDigits.test(name) || hasDigits.test(surname)) {
+        validationMessage.innerText = "Błąd: Imię i nazwisko nie mogą zawierać cyfr!";
+        validationMessage.classList.add('error-text');
+        return;
+    }
+
+    if (!email.includes('@') || !email.includes('.')) {
+        validationMessage.innerText = "Błąd: Wprowadź poprawny adres e-mail!";
+        validationMessage.classList.add('error-text');
+        return;
+    }
+
+    validationMessage.innerText = "Formularz został wysłany pomyślnie!";
+    validationMessage.classList.add('success-text');
+    
+    contactForm.reset();
+});
